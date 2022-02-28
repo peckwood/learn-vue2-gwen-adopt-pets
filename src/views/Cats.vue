d3d581d8812ecc85fec502883eb4f8cbf204ec95
raiden <peckwood@users.noreply.github.com>
2022-03-01 00:07:06 +0800

    minor improvements for cats

--- src/views/Cats.vue
+++ src/views/Cats.vue
@@ -1,18 +1,19 @@
 <template>
   <div>
+    <h1>Cats for adoption</h1>
     <b-table striped hover :items="cats"></b-table>
   </div>
 </template>
 
 <script>
 // @ is a shortcut to src folder
-import cats from "@/data/cats";
+import cats from '@/data/cats'
 export default {
-  data() {
+  data () {
     return {
       cats
-      //this is a shorthand to 'cats: cats'
-    };
-  },
-};
+      // this is a shorthand to 'cats: cats'
+    }
+  }
+}
 </script>
