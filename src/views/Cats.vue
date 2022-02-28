222d96c5fe20621c4017545901faebeb8945a878
raiden <peckwood@users.noreply.github.com>
2022-02-28 23:56:25 +0800

    Can now see cats table

--- src/views/Cats.vue
+++ src/views/Cats.vue
@@ -1,5 +1,18 @@
 <template>
-    <div>
-        Cats
-    </div>
+  <div>
+    <b-table striped hover :items="cats"></b-table>
+  </div>
 </template>
+
+<script>
+// @ is a shortcut to src folder
+import cats from "@/data/cats";
+export default {
+  data() {
+    return {
+      cats
+      //this is a shorthand to 'cats: cats'
+    };
+  },
+};
+</script>
