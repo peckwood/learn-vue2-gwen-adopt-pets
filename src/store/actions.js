1c5519a4ee22e61cf3a7579eb88e3d7c041e3ba9
fengxiao <fengxiao@sxylzinfo.com>
2022-03-03 18:00:11 +0800

    added toggleable form

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,2 +1,5 @@
 export default {
+  addPet: ({ commit }, payload) => {
+    commit('appendPet', payload)
+  }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,41 @@
 <template>
   <div class="home">
     <h1>Adopt a new best friend</h1>
+    <button class="btn btn-primary" @click="togglePetForm">Add New Pet</button>
+
+      <b-form @submit="handleSubmit" v-show="showPetForm">
+
+      <b-form-group id="input-group-2" label="Pet's Name:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="formData.name"
+          placeholder="Enter name"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-3" label="Species:" label-for="input-3">
+        <b-form-select
+          id="input-3"
+          v-model="formData.species"
+          :options="['cats', 'dogs']"
+          required
+        ></b-form-select>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Pet's Age:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          type="number"
+          v-model="formData.age"
+          placeholder="Enter age"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
   </div>
 </template>
 
@@ -9,6 +44,24 @@
 // import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home'
+  name: 'Home',
+  data () {
+    return {
+      showPetForm: false,
+      formData: {
+        name: '',
+        age: 0,
+        species: null
+      }
+    }
+  },
+  methods: {
+    togglePetForm () {
+      this.showPetForm = !this.showPetForm
+    },
+    handleSubmit () {
+
+    }
+  }
 }
 </script>
